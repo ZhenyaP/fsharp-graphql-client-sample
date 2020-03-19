2246f59c3274a058eb0c5cddf7f479747b843651
Andrii Chebukin <XperiAndri@live.ru>
2020-03-19 02:16:07 +0200

    Added script to debug type provider

---
+++ BibliotekoClient/BibliotekoClient/Script.fsx
@@ -0,0 +1,6 @@
+﻿#r @"C:\Users\Andrii\Dev\GitHub\FSharp.Data.GraphQL\src\FSharp.Data.GraphQL.Client\bin\Debug\net461\FSharp.Data.GraphQL.Shared.dll"
+#r @"C:\Users\Andrii\Dev\GitHub\FSharp.Data.GraphQL\src\FSharp.Data.GraphQL.Client\bin\Debug\net461\FSharp.Data.GraphQL.Client.dll"
+#load @"Domain.fs"
+#load @"GraphQLProviderRequests.fs"
+
+GraphQLProviderRequests.asyncQueryRegistriByIsbn() |> Async.RunSynchronously |> printfn "%A"
