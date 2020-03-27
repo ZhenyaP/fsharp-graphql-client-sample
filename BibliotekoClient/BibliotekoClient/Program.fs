b2a4b9028fdb272d9e7b843f1d6fdcc0842d87af
Eugene Petrovich <zhpetrovich@yahoo.com>
2020-03-27 13:29:43 +0300

    Some new tests for Error Handling

--- BibliotekoClient/BibliotekoClient/Program.fs
+++ BibliotekoClient/BibliotekoClient/Program.fs
@@ -45,6 +45,23 @@ module Main =
         let recenzoId = "17ff91d1-945d-4358-bcef-ebe7a91df8ca"
         removeRecenzo recenzoId ""
 
+        // Here we are correctly adding a new review with reaction, and after that
+        // we are trying to remove a comment from this review, and it should fail
+        let isbn = "9781680502541"
+        let recenzoId = "7dabec3d-1370-466e-a7a7-993ef3177297"
+        let recenzoKind = BiblProvider.Types.ReactionEnum.Gripping
+        setReaction isbn recenzoId recenzoKind ""
+        removeComment recenzoId ""
+
+        // Here we are correctly adding a new review with comment, and after that
+        // we are trying to remove a reaction from this review, and it should fail
+        let isbn = "978-1617291326"
+        let recenzoId = "72f992de-3705-4610-861e-9447cb1e8d82"
+        let comment = "It is so boring"
+        setComment isbn recenzoId comment ""
+        removeReaction recenzoId ""
+
+
     let callViaGraphQLProvider () =
         printHeader "Calls Via GraphQLProvider:"
 
